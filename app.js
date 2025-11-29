5bcf5a2ff5c6403a88b6ed9aed88eaa03ed08d73
SpotstatsAi <dblair1027@gmail.com>
2025-11-29 18:32:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -172,3 +172,13 @@ document.querySelectorAll(".filterButton").forEach(btn => {
 });
 
 document.getElementById("loadButton").onclick = loadData;
+
+.seasonAverages {
+  width: 22%;
+  font-size: 0.85em;
+  opacity: 0.9;
+}
+
+.seasonAverages div {
+  line-height: 1.3em;
+}
